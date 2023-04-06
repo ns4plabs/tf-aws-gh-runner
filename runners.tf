c225e9263cd3c058f23e92bb16ced07e9d050c96
galargh <piotr.galar@gmail.com>
2023-04-06 10:14:37 +0200

    allow boxo

--- runners.tf
+++ runners.tf
@@ -51,6 +51,32 @@ module "runners" {
         snapshot_id           = null
       }]
     }
+    "boxo" = {
+      runner_os = "linux"
+      runner_architecture = "x64"
+      instance_types = ["c5.4xlarge"]
+      repository_white_list = ["pl-strflt/tf-aws-gh-runner", "ipfs/boxo"]
+      runners_maximum_count = 20
+      instance_target_capacity_type = "on-demand"
+      # TODO: change to an AMI built specifically for boxo
+      ami_filter = { name = ["github-runner-ubuntu-jammy-amd64-202212300856-kubo"] }
+      ami_owners = ["642361402189"]
+      enabled_userdata = false
+      enable_runner_binaries_syncer = false
+      enable_runner_detailed_monitoring = true
+      runner_run_as = "ubuntu"
+      block_device_mappings = [{
+        device_name           = "/dev/sda1"
+        delete_on_termination = true
+        volume_type           = "io2"
+        volume_size           = 100
+        encrypted             = true
+        iops                  = 2500
+        throughput            = null
+        kms_key_id            = null
+        snapshot_id           = null
+      }]
+    }
   }
 
   source                          = "philips-labs/github-runner/aws"
