3228e846128e38fc001483fad415ec983a16b0bd
galargh <piotr.galar@gmail.com>
2023-04-11 14:07:28 +0200

    restore old ami for kubo

--- runners.tf
+++ runners.tf
@@ -8,7 +8,7 @@ module "runners" {
       repository_white_list = ["pl-strflt/tf-aws-gh-runner", "galorgh/kubo", "ipfs/kubo"]
       runners_maximum_count = 20
       instance_target_capacity_type = "on-demand"
-      ami_filter = { name = ["github-runner-ubuntu-jammy-amd64-202304081735-default"] }
+      ami_filter = { name = ["github-runner-ubuntu-jammy-amd64-202212300856-kubo"] }
       ami_owners = ["642361402189"]
       enabled_userdata = false
       enable_runner_binaries_syncer = false
