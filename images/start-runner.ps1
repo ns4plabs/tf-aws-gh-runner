9527611ec3d2b109123e7119206db3669ba1acec
galargh <piotr.galar@gmail.com>
2023-08-23 18:48:23 +0200

    chore: restore philips-labs defaults for windows runner

--- images/start-runner.ps1
+++ images/start-runner.ps1
@@ -4,25 +4,38 @@ Start-Transcript -Path "C:\runner-startup.log" -Append
 
 Write-Host  "Retrieving TOKEN from AWS API"
 $token=Invoke-RestMethod -Method PUT -Uri "http://169.254.169.254/latest/api/token" -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" = "180"}
+if ( ! $token ) {
+  $retrycount=0
+  do {
+    echo "Failed to retrieve token. Retrying in 5 seconds."
+    Start-Sleep 5
+    $token=Invoke-RestMethod -Method PUT -Uri "http://169.254.169.254/latest/api/token" -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" = "180"}
+    $retrycount=$retrycount + 1
+    if ( $retrycount -gt 40 )
+    {
+        break
+    }
+  } until ($token)
+}
 
 $ami_id=Invoke-RestMethod -Uri "http://169.254.169.254/latest/meta-data/ami-id" -Headers @{"X-aws-ec2-metadata-token" = $token}
 
 $metadata=Invoke-RestMethod -Uri "http://169.254.169.254/latest/dynamic/instance-identity/document" -Headers @{"X-aws-ec2-metadata-token" = $token}
 
 $Region = $metadata.region
-Write-Host  "Reteieved REGION from AWS API ($Region)"
+Write-Host  "Retrieved REGION from AWS API ($Region)"
 
 $InstanceId = $metadata.instanceId
-Write-Host  "Reteieved InstanceId from AWS API ($InstanceId)"
+Write-Host  "Retrieved InstanceId from AWS API ($InstanceId)"
 
 $tags=aws ec2 describe-tags --region "$Region" --filters "Name=resource-id,Values=$InstanceId" | ConvertFrom-Json
 Write-Host  "Retrieved tags from AWS API"
 
 $environment=$tags.Tags.where( {$_.Key -eq 'ghr:environment'}).value
-Write-Host  "Reteieved ghr:environment tag - ($environment)"
+Write-Host  "Retrieved ghr:environment tag - ($environment)"
 
 $runner_name_prefix=$tags.Tags.where( {$_.Key -eq 'ghr:runner_name_prefix'}).value
-Write-Host  "Reteieved ghr:runner_name_prefix tag - ($runner_name_prefix)"
+Write-Host  "Retrieved ghr:runner_name_prefix tag - ($runner_name_prefix)"
 
 $ssm_config_path=$tags.Tags.where( {$_.Key -eq 'ghr:ssm_config_path'}).value
 Write-Host  "Retrieved ghr:ssm_config_path tag - ($ssm_config_path)"
@@ -48,48 +61,6 @@ if ($enable_cloudwatch_agent -eq "true")
     Write-Host  "Enabling CloudWatch Agent"
     & 'C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1' -a fetch-config -m ec2 -s -c "ssm:$ssm_config_path/cloudwatch_agent_config_runner"
 }
-
-## Configure docker registries
-
-$docker_parameters=$(aws ssm get-parameters-by-path --path "/tf-aws-gh-runner/docker" --region "$Region" --query "Parameters[*].{Name:Name,Value:Value}") | ConvertFrom-Json
-echo "Retrieved docker parameters from AWS SSM"
-
-### Docker Registry Proxy
-
-$docker_proxy=$docker_parameters.where( {$_.Name -eq "/tf-aws-gh-runner/docker/proxy_aws_lb_dns_name"} ).value
-echo "Retrieved /tf-aws-gh-runner/docker/proxy_aws_lb_dns_name parameter - ($docker_proxy)"
-
-#### Docker Daemon Configuration
-$dockerConfigPath = "C:\ProgramData\Docker\config\daemon.json"
-$dockerConfig = @{
-    'insecure-registries' = @($docker_proxy)
-    'registry-mirrors' = @("http://$docker_proxy")
-}
-$dockerConfig | ConvertTo-Json | Set-Content -Path $dockerConfigPath
-
-##### BuildKit Configuration - https://github.com/moby/buildkit/issues/616
-# $buildkitConfigPath = "C:\ProgramData\BuildKit\config\buildkitd.toml"
-# $buildkitConfig = @"
-# insecure-entitlements = [
-#   "network.host",
-#   "security.insecure"
-# ]
-# [registry."docker.io"]
-#   mirrors = [
-#     "$docker_proxy"
-#   ]
-# [registry."$docker_proxy"]
-#   http = true
-#   insecure = true
-# "@
-# $buildkitConfig | Set-Content -Path $buildkitConfigPath
-
-Restart-Service -Name 'com.docker.service'
-
-### Go Modules Proxy
-
-$goproxy=$docker_parameters.where( {$_.Name -eq "/tf-aws-gh-runner/docker/goproxy_aws_lb_dns_name"} ).value
-echo "Retrieved /tf-aws-gh-runner/docker/goproxy_aws_lb_dns_name parameter - ($goproxy)"
 
 ## Configure the runner
 
@@ -141,6 +112,8 @@ $configCmd = ".\config.cmd --unattended --name $runner_name_prefix$InstanceId --
 Write-Host "Configure GH Runner as user $run_as"
 Invoke-Expression $configCmd
 
+Write-Host "Starting the runner as user $run_as"
+
 $jsonBody = @(
     @{
         group='Runner Image'
@@ -149,48 +122,12 @@ $jsonBody = @(
 )
 ConvertTo-Json -InputObject $jsonBody | Set-Content -Path "$pwd\.setup_info"
 
+Write-Host  "Installing the runner as a service"
+
+$action = New-ScheduledTaskAction -WorkingDirectory "$pwd" -Execute "run.cmd"
+$trigger = Get-CimClass "MSFT_TaskRegistrationTrigger" -Namespace "Root/Microsoft/Windows/TaskScheduler"
+Register-ScheduledTask -TaskName "runnertask" -Action $action -Trigger $trigger -User $username -Password $password -RunLevel Highest -Force
+Write-Host "Starting the runner in persistent mode"
 Write-Host "Starting runner after $(((get-date) - (gcim Win32_OperatingSystem).LastBootUpTime).tostring("hh':'mm':'ss''"))"
-Write-Host "Starting the runner as user $run_as"
-
-if ($agent_mode -eq "ephemeral")
-{
-  $startRunnerServicePath = "C:\start-runner-service.ps1"
-  $startRunnerService = @"
-    Write-Host "Starting the runner in ephemeral mode"
-
-    `$env:GOPROXY = "http://$goproxy,direct"
-
-    `$credentials = New-Object System.Management.Automation.PSCredential ("$username", $securePassword)
-
-    Start-Process -FilePath "run.cmd" -Credential `$credentials -WorkingDirectory "$($pwd.Path)" -Wait
-
-    `$exitCode = `$LASTEXITCODE
-    Write-Host "Runner has finished with `$exitCode exit code"
-
-    # Ideas for further improvements:
-    # - Check if /var/runner-startup.log contains Listening for Jobs
-    # - Check exit code of ./run.sh
-
-    Write-Host "Wait for 30 seconds to ensure all logs are flushed"
-    Start-Sleep -Seconds 30
-
-    Write-Host "Stopping cloudwatch service"
-    & 'C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1' -a stop
-
-    Write-Host "Terminating instance"
-    aws ec2 terminate-instances --instance-ids "$InstanceId" --region "$Region"
-  "@
-
-  $startRunnerService | Set-Content -Path $startRunnerServicePath
-
-  Start-Job -FilePath "$startRunnerServicePath" -Name "start-runner-service"
-}
-else {
-  Write-Host "Starting the runner in persistent mode"
-  $action = New-ScheduledTaskAction -WorkingDirectory "$pwd" -Execute "run.cmd"
-  $trigger = Get-CimClass "MSFT_TaskRegistrationTrigger" -Namespace "Root/Microsoft/Windows/TaskScheduler"
-  Register-ScheduledTask -TaskName "runnertask" -Action $action -Trigger $trigger -User $username -Password $password -RunLevel Highest -Force
-  Write-Host "Starting the runner in persistent mode"
-}
 
 Stop-Transcript
